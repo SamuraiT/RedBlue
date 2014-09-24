4374e7f52bfa27878736df9b63fe330ba09f92c7
SamuraiT <t.yasukawa01@gmail.com>
2014-09-24 20:44:14 +0900

    add another test

--- RedBlue/EventInfoExtractor.py
+++ RedBlue/EventInfoExtractor.py
@@ -30,6 +30,27 @@ class Event(object):
     87.5
     >>> e.postal_code
     '0700033'
+    >>> e.datels == [{'label': '開催', 'span': (434, 436)}, \
+                    {'label': '開催', 'span': (512, 514)}, \
+                    {'label': '開催日', 'span': (562, 565)}]
+    True
+    >>> e.placels == [{'label': '開催', 'span': (434, 436)}, \
+                      {'label': '会場', 'span': (472, 474)}, \
+                      {'label': '開催場所', 'span': (512, 516)}, \
+                      {'label': '開催', 'span': (562, 564)}]
+    True
+    >>> e.duris == [{'label': ['13:00', '13:05'], 'span': (109, 120)}, \
+                    {'label': ['13:05', '17:00'], 'span': (133, 144)},\
+                    {'label': ['13:00', '17:00'], 'span': (578, 589)}]
+    True
+    >>> e.times == [{'label': '13:00', 'span': (109, 114)}, \
+                    {'label': '13:05', 'span': (115, 120)}, \
+                    {'label': '13:05', 'span': (133, 138)}, \
+                    {'label': '17:00', 'span': (139, 144)}, \
+                    {'label': '2時', 'span': (336, 338)}, \
+                    {'label': '13:00', 'span': (578, 583)}, \
+                    {'label': '17:00', 'span': (584, 589)}]
+    True
     """
 
     def __init__(self, text=None):
@@ -208,6 +229,4 @@ class Event(object):
             return date
         except ValueError:
             pass
-        return date 
-
-
+        return date
